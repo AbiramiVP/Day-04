b5e2144388498d8df6ff2b4c5bc3a0f337a9c710
Abirami P <146667433+AbiramiVP@users.noreply.github.com>
2024-08-14 11:47:02 +0530

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,23 @@
+//02) create own resume in json fromat ..............................
+
+{
+  "name": "Abirami",
+  "mail": "abirami.official@gmail.com",
+  "mob": "9876543210",
+  "skills": ["HTML", "CSS", "JavaScript", "Java", "MySQL", "Figma"],
+  "address": "Tambaram, Chennai",
+  "education": [
+    {
+      "level": "UG",
+      "degree": "MCS",
+      "institution": "SRM Institute of Science and Technology, Ramapuram",
+      "cgpa": 9.3
+    },
+    {
+      "level": "PG",
+      "degree": "MCS",
+      "institution": "SRM Institute of Science and Technology, Ramapuram",
+      "cgpa": 8.3
+    }
+  ]
+}
